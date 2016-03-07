c5b7b808879ce3f026eff745231864dd58e9d529
sempeck <jarek.sepek@mediorama.pl>
2016-03-07 15:21:12 +0100

    Zadanie 3

--- lab2/lab3.c
+++ lab2/lab3.c
@@ -1,2 +1,50 @@
 // Zadanie 3 (4 pkt.)
-// Napisz program, który wczyta 15 liczb różnych liczb całkowitych do jednowymiarowej tablicy, a następnie znajduje największą i najmniejszą z nich, a także ich pozycje w zbiorze.
+// Napisz program, który wczyta 15 różnych liczb całkowitych do jednowymiarowej tablicy, a następnie znajduje największą i najmniejszą z nich, a także ich pozycje w zbiorze.
+
+#include <stdio.h>
+
+int main () {
+  int n;
+  int i = 0;
+  int tab[15];
+
+// wpisywanie
+  while (i < 15) {
+    printf("Podaj liczbę nr %d:\n", i);
+    scanf("%d", &n);
+    tab[i] = n;
+    for (int j = 0; j < i; j++) {
+      if (n == tab[j]) {
+        printf("Ta liczba już była. Podaj inną.\n");
+        i--;
+      };
+    };
+    i++;
+  };
+
+// max i min
+  int min = tab[0];
+  int minIndex = 0;
+  int max = 0;
+  int maxIndex;
+
+  for (i=0;i<15;i++) {
+    if (tab[i] > max) {
+      max = tab[i];
+      maxIndex = i;
+    };
+    if (tab[i] < min) {
+      min = tab[i];
+      minIndex = i;
+    }
+
+    printf("liczba %d: %d\n", i, tab[i]);
+  }
+
+// wynik 
+  printf("Najmniejsza liczba: %d, jej indeks: %d\n", min, minIndex);
+  printf("Największa liczba: %d, jej indeks: %d\n", max, maxIndex);
+
+  getchar();
+  getchar();
+}
--- lab2/lab4.c
+++ lab2/lab4.c
@@ -1,7 +1,15 @@
 // Zadanie 4 (8 pkt.)
-// Napisz program umożliwiający realizację następujących zadań na macierzy kwadratowej o ustalonym dowolnym
-// wymiarze:
+// Napisz program umożliwiający realizację następujących zadań na macierzy kwadratowej o ustalonym dowolnym wymiarze:
 //  Wyznaczenie sumy wartości poniżej przekątnej
 //  Wyznaczenie sumy wartości powyżej przekątnej
 //  Wyznaczenie sumy we wskazanym przez użytkownika wierszu lub kolumnie
 // Zawartość i analizowany zakres macierzy wczytywane mają być od użytkownika (maksymalny rozmiar jest stały, dowolnie ustalony przez programistę użytkownik może jednak chcieć pracować np. tylko na macierzy 3x3). Poza wyliczonymi sumami wyświetl również samą macierz po każdorazowej operacji. Program kończy swoje działanie dopiero po jednoznacznym wskazaniu tego przez użytkownika – pozwala wielokrotnie wyliczać poszczególne sumy
+
+
+switch (x)
+ {
+ case 1:printf("Jedynka");break;
+ case 2:printf("Dwojka");break;
+ case 3:printf("Trojka");break;
+ default :printf("Nie ten zakres");
+ }
